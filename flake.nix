dae189ee3bc366559f9c421db9e8e9b944f3f8de
vtor <iamvtor@gmail.com>
2026-02-06 12:48:57 -0800

    Implement robust build strategy (fetch dependencies as Nix inputs)

--- flake.nix
+++ flake.nix
@@ -3,9 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Component Sources
+    sls-src = { url = "github:irlserver/irl-srt-server"; flake = false; };
+    srt-src = { url = "github:irlserver/srt/belabox"; flake = false; };
+    srtla-src = { url = "github:irlserver/srtla"; flake = false; };
+
+    # SLS Submodule Alternatives (to fix recursive fetch issues)
+    spdlog-src = { url = "github:irlserver/spdlog/1.9.2"; flake = false; };
+    json-src = { url = "github:nlohmann/json"; flake = false; };
+    thread-pool-src = { url = "github:bshoshany/thread-pool"; flake = false; };
+    httplib-src = { url = "github:yhirose/cpp-httplib"; flake = false; };
+    cxxurl-src = { url = "github:chmike/CxxUrl"; flake = false; };
+    
+    # SRTLA Submodule Alternatives
+    argparse-src = { url = "github:p-ranav/argparse"; flake = false; };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, sls-src, srt-src, srtla-src, spdlog-src, json-src, thread-pool-src, httplib-src, cxxurl-src, argparse-src }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -17,11 +32,10 @@
           pkgs = pkgsFor system;
         in
         rec {
-          # 1. Patched SRT Library (belabox branch)
           libsrt-belabox = pkgs.stdenv.mkDerivation {
             pname = "libsrt-belabox";
             version = "unstable";
-            src = ./srt;
+            src = srt-src;
             nativeBuildInputs = [ pkgs.cmake pkgs.tcl pkgs.pkg-config ];
             buildInputs = [ pkgs.openssl pkgs.zlib ];
             cmakeFlags = [
@@ -30,38 +44,32 @@
               "-DENABLE_STATIC=OFF"
               "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
             ];
-            # Fix malformed .pc files (double slashes / incorrect prefixing)
             postInstall = ''
               find $out -name "*.pc" -exec sed -i "s|//|/|g" {} +
-              find $out -name "*.pc" -exec sed -i "s|libdir=.*|libdir=$out/lib|g" {} +
-              find $out -name "*.pc" -exec sed -i "s|includedir=.*|includedir=$out/include|g" {} +
             '';
           };
 
-          # 2. SRTLA Utilities
           srtla = pkgs.stdenv.mkDerivation {
             pname = "srtla";
             version = "unstable";
-            src = ./srtla;
+            src = srtla-src;
             nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config ];
-            buildInputs = [ libsrt-belabox pkgs.openssl pkgs.spdlog pkgs.argparse ];
+            buildInputs = [ libsrt-belabox pkgs.openssl ];
             
-            # Patch CMake to use system libraries instead of FetchContent
-            # This keeps srtla/ folder clean for 'git pull'
             postPatch = ''
-              substituteInPlace CMakeLists.txt \
-                --replace-fail "include(FetchContent)" "find_package(spdlog REQUIRED)" \
-                --replace-fail '"deps/argparse/include"' ""
-              
-              # Remove the multi-line FetchContent_Declare block
-              sed -i '/FetchContent_Declare/,/)/d' CMakeLists.txt
-              # Remove FetchContent_MakeAvailable line
-              sed -i '/FetchContent_MakeAvailable/d' CMakeLists.txt
+              # Assemble dependencies into the locations CMake expects
+              rm -rf deps/argparse deps/spdlog
+              mkdir -p deps/argparse deps/spdlog
+              cp -r ${argparse-src}/* deps/argparse/
+              cp -r ${spdlog-src}/* deps/spdlog/
+              chmod -R +w deps/
             '';
 
-            cmakeFlags = [
+            # Force CMake to use our manually assembled deps instead of fetching
+            cmakeFlags = [ 
+              "-DSPDLOG_BUILD_SHARED=OFF" 
+              "-DFETCHCONTENT_SOURCE_DIR_SPDLOG=../deps/spdlog"
               "-DFETCHCONTENT_FULLY_DISCONNECTED=ON"
-              "-DSPDLOG_BUILD_SHARED=ON"
             ];
             installPhase = ''
               mkdir -p $out/bin
@@ -69,18 +77,25 @@
             '';
           };
 
-          # 3. SRT Live Server (SLS)
           sls = pkgs.stdenv.mkDerivation {
             pname = "srt-live-server";
             version = "unstable";
-            src = ./sls;
+            src = sls-src;
             nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config ];
-            buildInputs = [
-              libsrt-belabox
-              pkgs.openssl
-              pkgs.zlib
-            ];
-            # Point CMake to the libsrt we just built
+            buildInputs = [ libsrt-belabox pkgs.openssl pkgs.zlib ];
+
+            postPatch = ''
+              # Assemble dependencies
+              rm -rf lib/spdlog lib/json lib/thread-pool lib/cpp-httplib lib/CxxUrl
+              mkdir -p lib/spdlog lib/json lib/thread-pool lib/cpp-httplib lib/CxxUrl
+              cp -r ${spdlog-src}/* lib/spdlog/
+              cp -r ${json-src}/* lib/json/
+              cp -r ${thread-pool-src}/* lib/thread-pool/
+              cp -r ${httplib-src}/* lib/cpp-httplib/
+              cp -r ${cxxurl-src}/* lib/CxxUrl/
+              chmod -R +w lib/
+            '';
+
             cmakeFlags = [
               "-DSRT_INCLUDE_DIR=${libsrt-belabox}/include"
               "-DSRT_LIBRARY=${libsrt-belabox}/lib/libsrt.so"
@@ -96,63 +111,29 @@
         });
 
       apps = forAllSystems (system: {
-        sls = {
-          type = "app";
-          program = "${self.packages.${system}.sls}/bin/srt_server";
-        };
-        srtla = {
-          type = "app";
-          program = "${self.packages.${system}.srtla}/bin/srtla_rec";
-        };
+        sls = { type = "app"; program = "${self.packages.${system}.sls}/bin/srt_server"; };
+        srtla = { type = "app"; program = "${self.packages.${system}.srtla}/bin/srtla_rec"; };
       });
 
       nixosModules.default = { config, lib, pkgs, ... }:
         let
           cfg = config.services.irl-srt-server;
           system = pkgs.stdenv.hostPlatform.system;
-          # Access the packages from the flake's outputs for the current system
           flakePkgs = self.packages.${system};
         in
         {
           options.services.irl-srt-server = {
             enable = lib.mkEnableOption "IRL Streaming Server (SLS)";
-            
-            package = lib.mkOption {
-              type = lib.types.package;
-              default = flakePkgs.sls;
-              description = "The SLS server package to use.";
+            package = lib.mkOption { type = lib.types.package; default = flakePkgs.sls; };
+            configPath = lib.mkOption { 
+              type = lib.types.path; 
+              default = "${sls-src}/src/sls.conf"; 
             };
-
-            configPath = lib.mkOption {
-              type = lib.types.path;
-              default = ./sls/src/sls.conf;
-              description = "Path to the sls.conf file.";
-            };
-
             srtla = {
-              enable = lib.mkOption {
-                type = lib.types.bool;
-                default = true;
-                description = "Whether to enable the SRTLA receiver proxy.";
-              };
-
-              package = lib.mkOption {
-                type = lib.types.package;
-                default = flakePkgs.srtla;
-                description = "The SRTLA package to use.";
-              };
-
-              port = lib.mkOption {
-                type = lib.types.port;
-                default = 5000;
-                description = "Port to bind the SRTLA socket to.";
-              };
-
-              forwardPort = lib.mkOption {
-                type = lib.types.port;
-                default = 4002;
-                description = "The SLS port to forward bonded traffic to.";
-              };
+              enable = lib.mkOption { type = lib.types.bool; default = true; };
+              package = lib.mkOption { type = lib.types.package; default = flakePkgs.srtla; };
+              port = lib.mkOption { type = lib.types.port; default = 5000; };
+              forwardPort = lib.mkOption { type = lib.types.port; default = 4002; };
             };
           };
 
@@ -181,37 +162,9 @@
               };
             };
 
-            # Open default ports in the firewall
             networking.firewall.allowedUDPPorts = [ 4000 4001 4002 ] ++ (if cfg.srtla.enable then [ cfg.srtla.port ] else []);
             networking.firewall.allowedTCPPorts = [ 8181 ];
           };
         };
-
-      devShells = forAllSystems (system:
-        let
-          pkgs = pkgsFor system;
-          libs = self.packages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              cmake
-              gcc
-              pkg-config
-              openssl
-              zlib
-              tcl
-              libs.libsrt-belabox
-              libs.srtla
-              libs.sls
-            ];
-
-            shellHook = ''
-              export LD_LIBRARY_PATH="${self.packages.${system}.libsrt-belabox}/lib:$LD_LIBRARY_PATH"
-              echo "IRL Streaming Workspace Loaded"
-              echo "Packages available: srt_server, srt_client, srtla_rec"
-            '';
-          };
-        });
     };
 }
